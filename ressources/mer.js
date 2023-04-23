c1cbfb6c7bfe4b2b6a9581026b7fa653e16e3745
La Capsule <79408224+lacapsule@users.noreply.github.com>
2023-04-23 14:24:37 +0200

    Ajouts de contrôles de couleur pour la terre, le liseret et planification du module de contrôle de vue

--- ressources/mer.js
+++ ressources/mer.js
@@ -1,11 +1,14 @@
-let mer = document.getElementById("mer");
-couleurMer.addEventListener("input", () => {
-    mer.style.background = couleurMer.value;
-});
-
 liseret.addEventListener("click", () => {
     if (getComputedStyle(Liseret).display != "none")
         Liseret.style.display = "none";
     else
         Liseret.style.display = "block";
+});
+
+couleurMer.addEventListener("input", () => {
+    mer.style.background = couleurMer.value;
+});
+
+couleurLiseret.addEventListener("input", () => {
+    Liseret.style.stroke = couleurLiseret.value;
 });
---
+++ ressources/terre.js
@@ -0,0 +1,8 @@
+couleurCommunes.addEventListener("input", () => {
+    communes.style.fill = couleurCommunes.value;
+});
+
+couleurDepartements.addEventListener("input", () => {
+    Morbihan.style.fill = couleurDepartements.value;
+    Cotes_d_Armor.style.fill = couleurDepartements.value;
+});
