f66e62d394a7469f56b7a41db9442d7340b431db
lacapsule <info@lacapsule.org>
2023-04-26 20:45:24 +0200

    Correction du script

--- ressources/menuCommune.js
+++ ressources/menuCommune.js
@@ -1,20 +1,22 @@
 const villes = document.querySelectorAll('.commune');
 const body = document.querySelector('body');
-let nom = document.querySelector('h5');
+let name = document.querySelector('h5');
 let info = document.querySelector('.infos');
-let x = e.clientX + window.pageXOffset + -800;
-let y = e.clientY + window.pageYOffset + 75;
+
 
 villes.forEach(ville => {
     ville.addEventListener('contextmenu', function (e) {
         e.preventDefault();
+       var x = e.clientX + window.pageXOffset + -800;
+       var y = e.clientY + window.pageYOffset + 75;
        ville.style.fill = couleurCommune.value;
        console.log('La souris est positionné en : x=' + x + ' y=' + y);
 
        info.classList.toggle('show');
        info.style.left = x + 'px';
        info.style.top = y + 'px';
-       nom.innerHTML = ville.dataset.name;        
+       name.innerHTML = ville.dataset.name;
+
     });
 });
 
