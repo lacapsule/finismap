9a003e6f4bbc5cca65992ea5b1bf605755d0608d
lacapsule <info@lacapsule.org>
2023-04-26 22:59:56 +0200

    Ajout d'une modification des frontières via le menu général.

--- ressources/menuGeneral.js
+++ ressources/menuGeneral.js
@@ -2,10 +2,10 @@ let a = document.getElementsByTagName('aside')[0];
 let b = document.getElementById('menuGeneral');
 
 b.addEventListener("click", () => {
-    if(getComputedStyle(a).left === '-480px')
+    if(getComputedStyle(a).left === '-430px')
       a.style.left = "0";
     else
-      a.style.left = "-480px";
+      a.style.left = "-430px";
   })
 function fleche() {
     this.classList.toggle('ferme');
--- ressources/terre.js
+++ ressources/terre.js
@@ -6,3 +6,8 @@ couleurDepartements.addEventListener("input", () => {
     Morbihan.style.fill = couleurDepartements.value;
     Cotes_d_Armor.style.fill = couleurDepartements.value;
 });
+
+couleurFrontieres.addEventListener("input", () => {
+    communes.style.stroke = couleurFrontieres.value;
+    departements.style.stroke = couleurFrontieres.value;
+});
